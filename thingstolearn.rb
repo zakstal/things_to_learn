cef0e3becf2039f63289cb8c7b05aa7d142860fa
App Academy Student <appacademy@Apps-Mac-mini-22.local>
2014-12-03 17:34:56 -0500

    first commit

---
+++ thingstolearn.rb
@@ -0,0 +1,59 @@
+
+programming:
+
+khan academy
+https://www.khanacademy.org/math/probability/probability-and-combinatorics-topic
+https://www.khanacademy.org/math/probability/independent-dependent-probability
+https://www.khanacademy.org/math/integral-calculus/sequences_series_approx_calc
+
+simon whiteboard challenges
+https://github.com/appacademy/job-search-curriculum/blob/master/interview-prep/whiteboard-strategies.md
+
+Read and memorize these. You will get quizzed about them.
+https://github.com/appacademy/job-search-curriculum/blob/master/interview-prep/performance-cheat-sheet.md
+https://github.com/appacademy/job-search-curriculum/blob/master/interview-prep/browser-navigation.md
+
+big o notation
+
+interview:
+technical questions asked during interviews
+https://docs.google.com/spreadsheet/ccc?key=0AnnoREts_wUydHN3UGZfbDZIME1VTEY3Y3pUNWpZZGc&usp=drive_web#gid=0
+
+-how some experience built skills and how you can work well with people
+-want the story to be interesting
+-story: barriers i overcame, learn, problem solve, chased excellence
+-success stories: team learning problem solving
+-“I would like to tell you about a wonderful learning experience”
+-focus on where you flourish
+-connect to attitude, how built confidence
+-pull soft skills out of the stories
+
+
+Questions:
+
+- what do you do when not working
+- where do I see myself in blank years? what is my passion/motivation?
+- how do i do well in a team setting?
+- how do you differentiate yourself?
+- how do you do under pressure?
+- why should we hire you?
+- what to do about bad team member?
+
+
+
+simon lecture notes
+
+a. How to handle questions you’ve seen before.
+I often get this question. Obviously the ethical thing is to tell your interviewer you’ve seen the problem before and you’ll get points for that. Put on a sad face to show you were excited about solving a new problem.
+
+Furthermore, you won’t necessarily perform better on a question you’ve seen. You’ll probably solve it faster, but like I said students overrate speed. On the other hand, it’s much harder to show your interviewer how you think when you’re really just recalling a solution. Note that the easiest parts of a problem are also the hardest to remember. So even if you confound your interviewer, they’ll be left wondering why you breezed through the hardest part of the problem whiling stumbling on the rest. That can’t be good.
+
+b. How to handle ambiguous questions
+
+During the job search you’ll often be in a position where you probably know the answer to the question being asked, but you’re unsure what exactly your interviewer is getting at. Most important here is not to panic. This isn’t a failure on your part. This situation is common to all job interviews, especially technical ones where concepts are more complex.
+
+The difference between an experienced developer and a junior developer in these situations isn’t that the experienced one can divine what the interviewer is asking. Rather he/she will have the confidence to recognize that it’s the question that’s ambiguous, not him/her that’s ignorant. A junior developer is more at risk of shooting in the dark and coming off as if he/she doesn’t have the answer, when in fact he/she does.
+
+Don’t get defensive when you get an ambiguous question. Engage. Try to understand what it is they’re asking. If it’s genuine, you won’t look like you’re fishing for hints.
+
+Say you’re asked “What’s a GET request?”. You answer “GET is one of the standard verbs in HTTP”. They say “Sure, but what is a GET *request*?”. Now you’re confused. Ok, go through what you know about GET requests. Like all requests they’re strings. Does the interviewer want detail on how the string is structured? Does he/she want to know how it differs from other HTTP requests? Does he/she want to talk about what resources should be available to GET requests according to REST? These answers aren’t equally relevant, but since you’re there to show your interviewer how much you know about web development, you might as well bring them up.
